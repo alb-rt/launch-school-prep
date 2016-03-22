57d6da570d738aed2ac9110a6cd38cb84a2a83cf
Albert Ramirez <AlbertR@gmail.com>
2016-03-22 08:07:32 -0400

    cleaned up

--- exercise2.rb
+++ exercise2.rb
@@ -6,4 +6,22 @@ Use the modulo operator, division, or a combination of both to take a 4 digit nu
 3) the tens and 
 4) and the ones.
   
-end
+=end
+
+# this is the 4-digit number #
+puts 4321
+
+# the thousands #
+puts 4321 / 1000
+
+# the hundreds #
+puts 4321 % 1000 / 100
+
+# the tens #
+puts 4321 % 100 / 10
+
+# the ones #
+puts 4321 % 10
+
+
+
