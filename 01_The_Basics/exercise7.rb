2a178a966b75c9dedd4d3267d88eab5cc83f4440
Albert Ramirez <AlbertR@gmail.com>
2016-03-22 17:20:05 -0400

    done

---
+++ 01_The_Basics/exercise7.rb
@@ -0,0 +1,10 @@
+=begin
+
+What does the following error message tell you?
+
+SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+    from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+=end
+
+puts 'Missing a closing bracket; user may have used a closing parenthesis instead.'
