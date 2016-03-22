13978ecd660770e2770e25d1584c2a441d480e1f
Albert Ramirez <AlbertR@gmail.com>
2016-03-22 17:02:26 -0400

    exercises 3 and 4

--- 01_The_Basics/exercise3.rb
+++ 01_The_Basics/exercise3.rb
@@ -3,3 +3,18 @@
   Then use the puts command to make your program print out the year of each movie to the screen.
 =end
 
+# Movie titles
+
+movies = {  :slumdog_millionare => '2008', 
+            :the_hurt_locker => '2009',
+            :the_kings_speech => '2010',
+            :the_artist => '2011',
+            :argo => '2012'}
+
+puts movies[:slumdog_millionare]
+puts movies[:the_hurt_locker]
+puts movies[:the_kings_speech]
+puts movies[:the_artist]
+puts movies[:argo]
+
+  
---
+++ 01_The_Basics/exercise4.rb
@@ -0,0 +1,17 @@
+=begin
+
+Use the dates from the previous example and store them in an array. 
+Then make your program output the same thing as exercise 3.
+
+Dates: 2008, 2009, 2010, 2011, 2012
+
+=end
+
+movie_dates = [2008, 2009, 2010, 2011, 2012]
+
+puts movie_dates[0]
+puts movie_dates[1]
+puts movie_dates[2]
+puts movie_dates[3]
+puts movie_dates[4]
+
