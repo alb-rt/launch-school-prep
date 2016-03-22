20475c457d7f64a0dfe39f937fe2843471d8daaf
Albert Ramirez <AlbertR@gmail.com>
2016-03-22 08:19:04 -0400

    new exercise

---
+++ 01_The_Basics/exercise3.rb
@@ -0,0 +1,5 @@
+=begin
+  Write a program that uses a hash to store a list of movie titles with the year they came out. 
+  Then use the puts command to make your program print out the year of each movie to the screen.
+=end
+
