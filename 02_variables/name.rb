1ede6256c3c6ea57d1f28b99a789534629833f03
Albert Ramirez <AlbertR@gmail.com>
2016-03-22 21:25:20 -0400

    update

---
+++ 02_variables/name.rb
@@ -0,0 +1,4 @@
+puts 'hi, what is your name?'
+name = gets.chomp
+
+puts 'Hi ' + name + ', that is my mothers name!'
---
+++ 02_variables/variables_notes.rb
@@ -0,0 +1,26 @@
+# Variables store and label data in memory.
+# IMPT: be thoughtful when naming variables - needs to be accurately descriptive and understandable to another reader (sometimes this ‘other reader’ is you in the future)
+
+# Assign variables with “=“
+e.g. first_name = ‘Albert’
+
+=begin 
+IMPT: “=“ != “==“
+  “=“ assigns value
+  “==“ checks if things are equal
+    e.g. men == women 
+    => true
+    (contrary to what many coders might think - ha!)
+=end
+
+=begin
+Variables don’t like “auto-update” or something:
+  e.g.
+    a = 4
+    b = a
+    a = 7
+    b = 4 still
+=end
+
+# I (person typing in terminal, or textedit in this case) can make variables but Users can store info in variables too.
+
