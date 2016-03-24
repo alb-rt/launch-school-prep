0504397d7d989c864770cd1b7cb6864fb696fcfb
Albert Ramirez <AlbertR@gmail.com>
2016-03-23 22:52:07 -0400

    added part 3 with fun String!

--- 02_variables/name.rb
+++ 02_variables/name.rb
@@ -1,11 +1,28 @@
+# part 1
 puts 'Hi, what is your name?'
 name = gets.chomp
 
 puts 'Hi ' + name + ', that is my mothers name!'
 puts ""
 
+# part 2
 puts 'In case you needed it, here is your name 10 times in capital letters:'
 
 10.times do 
   puts name.upcase
 end
+
+# part 3
+puts ""
+puts "Sorry, can I get your first name again? Super forgetful today. All lowercase would be great."
+first_name = gets.chomp
+puts "Cool, thanks " + first_name + "."
+puts "Ok, how about your last name?"
+last_name = gets.chomp
+# (Can't wait to actually learn how the below works!)
+class String
+  def capitalize_first_char
+    self.sub(/^(.)/) {$1.capitalize }
+  end
+end
+puts "Ok great. So I have your full name as " + first_name.capitalize_first_char + " " + last_name.capitalize_first_char + "."
