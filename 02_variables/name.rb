cf3dd7e739ce1258adf79f9fbd1aa2fc7edfff23
Albert Ramirez <AlbertR@gmail.com>
2016-03-23 22:44:04 -0400

    name.rb part 2

--- 02_variables/name.rb
+++ 02_variables/name.rb
@@ -1,4 +1,11 @@
-puts 'hi, what is your name?'
+puts 'Hi, what is your name?'
 name = gets.chomp
 
 puts 'Hi ' + name + ', that is my mothers name!'
+puts ""
+
+puts 'In case you needed it, here is your name 10 times in capital letters:'
+
+10.times do 
+  puts name.upcase
+end
